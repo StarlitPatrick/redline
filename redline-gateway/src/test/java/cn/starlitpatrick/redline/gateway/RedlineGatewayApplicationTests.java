0f9cd42d546c0d67e7ae11fe34861e32ab64e2af
tianyuheng <tianyuheng2@outlook.com>
2022-12-03 17:16:34 +0800

    add module readline-gateway

---
+++ redline-gateway/src/test/java/cn/starlitpatrick/redline/gateway/RedlineGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.starlitpatrick.redline.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedlineGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
