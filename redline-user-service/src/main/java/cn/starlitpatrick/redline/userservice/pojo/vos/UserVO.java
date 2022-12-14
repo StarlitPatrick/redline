6282bfebead931acd6b685a43e13f9ef257348bc
tianyuheng <tianyuheng2@outlook.com>
2022-12-14 23:57:34 +0800

    update: UserVO

--- redline-user-service/src/main/java/cn/starlitpatrick/redline/userservice/pojo/vos/UserVO.java
+++ redline-user-service/src/main/java/cn/starlitpatrick/redline/userservice/pojo/vos/UserVO.java
@@ -14,4 +14,17 @@ import java.io.Serializable;
 public class UserVO implements Serializable {
     @Serial
     private static final long serialVersionUID = 7829095819526371277L;
+
+    private Long id;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 昵称
+     */
+    private String nickname;
+
 }
