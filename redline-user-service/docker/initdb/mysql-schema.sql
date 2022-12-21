0b571df05e7388b301990fb6be21641105b470c8
tianyuheng <tianyuheng2@outlook.com>
2022-12-21 21:52:26 +0800

    Modify user-db init

--- redline-user-service/docker/initdb/mysql-schema.sql
+++ redline-user-service/docker/initdb/mysql-schema.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS `user`
+CREATE TABLE `user`
 (
     `id`          BIGINT UNSIGNED NOT NULL COMMENT 'id',
     `username`    VARCHAR(60)     NOT NULL COMMENT '用户名',
-    `password`    VARCHAR(60)     NOT NULL COMMENT '密码',
+    `password`    VARCHAR(500)    NOT NULL COMMENT '密码',
     `nickname`    VARCHAR(60)     NOT NULL COMMENT '昵称',
     `create_time` DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_time`   DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `update_time` DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     PRIMARY KEY (`id`)
 ) COMMENT '用户表';
